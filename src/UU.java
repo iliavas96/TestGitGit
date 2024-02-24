14bccdc8048bc2523933aed7be1666e91849645c
Ilia Vasilenko <iliavas96@yandex.ru>
2024-02-24 11:38:11 +0300

    БЛЯЯЯЯЯЯЯЯЯЯЯЯ

--- src/UU.java
+++ src/UU.java
@@ -1,2 +1,14 @@
 public class UU {
+    private int i =5;
+
+    public void addInt () {
+        System.out.println("2++=2");
+    }
+
+    public static void main(String[] args) {
+        UU uu = new UU();
+        uu.addInt();
+
+    }
+
 }
