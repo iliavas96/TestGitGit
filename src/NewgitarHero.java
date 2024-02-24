771815c499cdee2fe51cbf637af0786c2033a4c7
Ilia Vasilenko <iliavas96@yandex.ru>
2024-02-24 18:09:18 +0300

    играем на барабанах

--- src/NewgitarHero.java
+++ src/NewgitarHero.java
@@ -2,5 +2,6 @@ public class NewgitarHero {
 
     public static void main(String[] args) {
         System.out.println("Играем крутой рок на гитаре");
+        System.out.println("Играем крутой рок на барабанах");
     }
 }
