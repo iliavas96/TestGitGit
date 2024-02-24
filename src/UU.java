0b632ee6d185e376cadf81b8c962c692799ab4ac
Ilia Vasilenko <iliavas96@yandex.ru>
2024-02-24 14:11:57 +0300

    Добавил TreeMap и stream

--- src/UU.java
+++ src/UU.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class UU {
     private int i =5;
 
@@ -11,6 +16,23 @@ public class UU {
     public static void main(String[] args) {
         UU uu = new UU();
         uu.addInt();
+
+        TreeMap<Integer, String> number_case = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        System.out.println("_________________________");
+
+        number_case.put(77, "Kuka");
+        number_case.put(17, "Lupa");
+        number_case.put(27, "Kola");
+        number_case.put(707, "Rola");
+
+        number_case.entrySet().stream()
+                .map(Map.Entry::getKey)
+                .forEach(System.out::println);
 
     }
 
